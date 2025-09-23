46d32c4263aec9c1cf2d7cec34affa3798933d7a
merra19 <15358838+merra19@users.noreply.github.com>
2025-09-23 20:32:30 +0200

    fix mkinitramfs

--- file-disk/mkinitramfs/post-install.sh
+++ file-disk/mkinitramfs/post-install.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+HOOKS="base modules udev automod filedisk systemd"
 
 pkg_postinst() {
-	for h in base modules udev automod; do
+	for h in $HOOKS; do
 		if [ -f /etc/mkinitramfs.d/$h.hook ]; then
-			install -m755 /usr/share/mkinitramfs/hooks/$h.hook /etc/mkinitramfs.d/$h.hook.spkgnew
+			rm -f /etc/mkinitramfs.d/$h.hook
+			install -m755 /usr/share/mkinitramfs/hooks/$h.hook /etc/mkinitramfs.d/$h.hook
 		else
 			install -m755 /usr/share/mkinitramfs/hooks/$h.hook /etc/mkinitramfs.d/$h.hook
 		fi
@@ -12,7 +14,11 @@ pkg_postinst() {
 }
 
 pkg_preremove() {
-    pkg_postinst
+	for h in $HOOKS; do
+		if [ -f /etc/mkinitramfs.d/$h.hook ]; then
+			rm -f /etc/mkinitramfs.d/$h.hook
+		fi
+	done
 }
 
 case $1 in
