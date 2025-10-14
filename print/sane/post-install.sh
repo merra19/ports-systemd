a5ce501f36be45098b8d25a849e3e9579c6b2b4b
merra19 <15358838+merra19@users.noreply.github.com>
2025-10-14 20:39:12 +0200

    add xsane ,hplip and deps

--- print/sane/post-install.sh
+++ print/sane/post-install.sh
@@ -4,6 +4,12 @@ pkg_preinst() {
     getent group scanner || groupadd -g 70 scanner
 }
 
+pkg_postinst() {
+	mkdir -p /run/lock/sane
+	chgrp -v scanner  /run/lock/sane
+}
+
 case $1 in
     preinst) pkg_preinst ;;
+	postinst) pkg_postinst ;;
 esac
