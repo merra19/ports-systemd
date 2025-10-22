7e3d4e4ed73fd9e17508b85834324edb6f44b05d
merra19 <15358838+merra19@users.noreply.github.com>
2025-10-22 09:33:25 +0200

    add bind

---
+++ server/bind/post-install.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+pkg_preinst() {
+	getent group named > /dev/null || groupadd -g 20 named 
+	getent passwd named > /dev/null || useradd -c "BIND Owner" -g named -s /bin/false -u 20 named 
+}
+
+pkg_postinst() {
+	if [ ! -f /etc/bind/rndc.key ]; then
+		rndc-confgen -a -b 512 -t /srv/named
+	fi
+
+	if [ ! -f /etc/resolv.conf.bak ]; then
+		cp /etc/resolv.conf /etc/resolv.conf.bak &&
+		cat > /etc/resolv.conf << "EOF"
+search <yourdomain.com>
+nameserver 127.0.0.1
+EOF
+	fi
+
+    cd /usr/share/blfs-bootscripts
+    make install-named
+}
+
+pkg_preremove() {
+    cd /usr/share/blfs-bootscripts
+    make uninstall-named
+}
+
+case $1 in
+	preinst) pkg_preinst ;;
+    postinst) pkg_postinst ;;
+    preremove) pkg_preremove ;;
+esac
