2c1ab4cbdeed1ac08ef880020f08b698646711cd
merra19 <15358838+merra19@users.noreply.github.com>
2025-12-22 23:23:16 +0100

    remove /run directory ,and init datatbase postgresql

--- server/postgresql/post-install.sh
+++ server/postgresql/post-install.sh
@@ -5,11 +5,6 @@ pkg_preinst() {
     getent passwd postgres > /dev/null || useradd -c "PostgreSQL Server" -g postgres -d /srv/pgsql/data -u 41 postgres
 }
 
-pkg_postinst() {
-    su - postgres -c '/usr/bin/initdb -D /srv/pgsql/data'
-}
-
 case $1 in
     preinst) pkg_preinst ;;
-    postinst) pkg_postinst ;;
 esac
